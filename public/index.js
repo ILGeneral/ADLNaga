611001d3d814380d65a02c0206f8dbb111fde106
ILGeneral <160194833+ILGeneral@users.noreply.github.com>
2024-12-10 16:34:41 +0800

    Update index.js

--- public/index.js
+++ public/index.js
@@ -161,7 +161,7 @@ try {
 
       console.log("Fetched post data:", data); // Log post data for each post
 
-      // Add the post's HTML with the delete button
+      // For posts      
       container.innerHTML += `
         <div class="postbox" id="post-${postId}">
           <h3>${data.postTitle}</h3>
@@ -254,23 +254,7 @@ onAuthStateChanged(auth, (user) => {
   }
 });
 
-//redirect to visit post
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const postId = urlParams.get('id');
 
-if (postId) {
-  const postRef = doc(db, "Categories", path, "posts", postId);
-  getDoc(postRef).then((docSnap) => {
-    if (docSnap.exists()) {
-      const data = docSnap.data();
-      document.getElementById('post-title').innerText = data.postTitle;
-      document.getElementById('post-content').innerText = data.postContent;
-    } else {
-      console.error("No such document!");
-    }
-  }).catch((error) => {
-    console.error("Error fetching post:", error);
-  });
-}
+
+
 
